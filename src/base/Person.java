0e3f059db349c046898bf89d01237824be99b31b
Ryan Kwon <rynkwn@gmail.com>
2015-08-05 03:57:21 -0400

    Created some standard setters and getters, and reformatted code appearance. Need to create gitignore pretty soon to ignore ~docs created by opening Word documents. Also added CALORIES constant to Person.

--- src/base/Person.java
+++ src/base/Person.java
@@ -1,6 +1,8 @@
 package base;
 
 public class Person {
+	
+	public static final double CALORIES_NEEDED_PER_DAY = 2000;
 
 	private String name;
 	private String faction;
@@ -14,12 +16,19 @@ public class Person {
 		name = "Bitzy";
 	}
 	
-	public void setName(String name){
-		this.name = name;
-	}
+	// Standard setters.
+	public void setName(String name){ this.name = name; }
+	public void setFaction(String faction){ this.faction = faction; }
+	public void setGender(boolean gender){ this.gender = gender; }
+	public void setHappiness(double happiness){ this.happiness = happiness; }
+	public void setStrength(double strength){ this.strength = strength; }
 	
-	public String name(){
-		return name;
-	}
+	// Standard getters.
+	public String name(){ return name; }
+	public String faction(){ return faction; }
+	public boolean gender(){ return gender; }
+	public double happiness(){ return happiness; }
+	public double strength(){ return strength; }
+	
 	
 }
