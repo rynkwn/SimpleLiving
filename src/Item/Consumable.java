c440bcd00fbbf6e85662e4856fbc858455781983
Ryan Kwon <rynkwn@gmail.com>
2017-01-19 00:45:51 -0500

    Started some work on the basic item system. Components sound very interesting.

--- src/Item/Consumable.java
+++ src/Item/Consumable.java
@@ -1,6 +1,14 @@
 package Item;
 
 public class Consumable extends Item {
+	
+	// What sort of consumables?
+	// In the short term: hungry and thirst management.
+	// In the longer term: drug cocktails for performance/otherwise.
+	// Status effects! Drunknesses. Poisons. E.t.c.
+	
+	private double quenching; // Thirst
+	private double satisfying; // Hunger
 
 	private String effect; // Type of effect.
 	private double intensity;
