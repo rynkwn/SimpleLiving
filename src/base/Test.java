970a88d5a624aa146e4b1a6bbbc13c64f190938b
Ryan Kwon <rynkwn@gmail.com>
2018-02-15 14:00:07 -0800

    Set up a testInfra item. Systems not yet in place to really get infrastructure to work much though.

--- src/base/Test.java
+++ src/base/Test.java
@@ -12,8 +12,28 @@ import world.*;
 import util.*;
 import behavior.*;
 
+import com.google.gson.Gson;
+
 public class Test {
 	public static void main(String[] args) {
-		
+
+		Gson gson = new Gson();
+
+		String name = "testInfra";
+		double weight = 10.0;
+		int quantity = 1;
+		String projectName = "test";
+		LaborPool lp = new LaborPool(0);
+		HashMap<String, Integer> rawMats = new HashMap<String, Integer>();
+		rawMats.put("T1", 1);
+
+		Macronutrient nutr = new Macronutrient(0);
+		HashMap<String, Integer> products = new HashMap<String, Integer>();
+		products.put("P1", 1);
+
+		Infrastructure infra = new Infrastructure(name, weight, quantity, projectName, lp, rawMats, nutr, products);
+
+		System.out.println(gson.toJson(infra));
+		// Creating an Infrastructure Object.
 	}
 }
